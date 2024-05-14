5aa5d70d9709237461db4c926d247780ade64780
mohamedbassiony <mohamedbassiony333@gmail.com>
2024-05-14 14:57:10 +0300

    update file

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -26,14 +26,13 @@ class DataController(BaseController):
     def generate_unique_filename(self, orig_file_name: str, project_id: str):
 
         random_key = self.generate_random_string()
-        project_path = ProjectController().get_project_path(project_id)
+        project_path = ProjectController().get_project_path(project_id=project_id)
 
         cleaned_file_name = self.get_clean_file_name(
             orig_file_name=orig_file_name
         )
 
         new_file_path = os.path.join(
-
             project_path,
             random_key + "_" + cleaned_file_name
 
@@ -42,12 +41,11 @@ class DataController(BaseController):
         while os.path.exists(new_file_path):
             random_key = self.generate_random_string()
             new_file_path = os.path.join(
-
                 project_path,
                 random_key + "_" + cleaned_file_name
 
             )      
-        return new_file_path    
+        return new_file_path, random_key + "_" + cleaned_file_name    
 
 
     def get_clean_file_name(self, orig_file_name: str):

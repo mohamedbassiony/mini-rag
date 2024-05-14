1201f010f1ab80e6865d8b344a95d7a02c091123
mohamedbassiony <mohamedbassiony333@gmail.com>
2024-05-14 15:26:26 +0300

    update generate_unique_filepath function

--- src/routes/data.py
+++ src/routes/data.py
@@ -35,7 +35,7 @@ async def upload_data(project_id: str, file: UploadFile,
         )
     
     project_dir_path = ProjectController().get_project_path(project_id=project_id)
-    file_path, file_id = data_controller.generate_unique_filename(
+    file_path, file_id = data_controller.generate_unique_filepath(
         orig_file_name=file.filename,
         project_id = project_id
     )

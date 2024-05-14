eaedc5753d95acd6e120eb6910e4152355195b54
mohamedbassiony <mohamedbassiony333@gmail.com>
2024-05-14 13:40:36 +0300

    update ResponseSignal class

--- src/models/enums/ResponseEnum.py
+++ src/models/enums/ResponseEnum.py
@@ -6,4 +6,4 @@ class ResponseSignal(Enum):
     FILE_TYPE_NOT_SUPPORTED = "file_type_not_supported"
     FILE_SIZE_EXCEEDED = "file_size_exceeded"
     FILE_UPLOAD_SUCCESS = "file_upload_success"
-    FILE_UPLOAD_SUCCESS = "failed"
+    FILE_UPLOAD_FAILED = "file_upload_failed"

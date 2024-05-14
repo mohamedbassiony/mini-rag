f56a0e6346f4354441e454e0829c09e6d068b55c
mohamedbassiony <mohamedbassiony333@gmail.com>
2024-05-14 15:26:10 +0300

    update by enhancing the generate_unique_filepath function

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -23,7 +23,7 @@ class DataController(BaseController):
         return True, ResponseSignal.FILE_VALIDATED_SUCCESS.value
     
         
-    def generate_unique_filename(self, orig_file_name: str, project_id: str):
+    def generate_unique_filepath(self, orig_file_name: str, project_id: str):
 
         random_key = self.generate_random_string()
         project_path = ProjectController().get_project_path(project_id=project_id)
